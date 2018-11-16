f66b45ba9901b9051e1d66b682fd518ce30126c0
angelIA98 <45030709+angelIA98@users.noreply.github.com>
2018-11-16 09:48:39 +0100

    Update ControladorMenu.java

--- Proyecto Memorion DI/Memorion/src/memorion/ControladorMenu.java
+++ Proyecto Memorion DI/Memorion/src/memorion/ControladorMenu.java
@@ -27,15 +27,30 @@ public class ControladorMenu implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent ae) {
         System.out.println(ae.getActionCommand());
-        switch(ae.getActionCommand()){
-            case "Easy":
-                vista.crearCartas(3,4);
+        switch (ae.getActionCommand()) {
+            case "Fácil":
+                if (vista.existeContador()) {
+                    vista.restablecerContador();
+                }else{
+                vista.crearContador();
+                }
+                vista.crearCartas(3, 4);
                 break;
             case "Normal":
-                vista.crearCartas(4,5);
+                if (vista.existeContador()) {
+                    vista.restablecerContador();
+                }else{
+                vista.crearContador();
+                }
+                vista.crearCartas(4, 5);
                 break;
-            case "Dificult":
-                vista.crearCartas(5,6);
+            case "Difícil":
+                if (vista.existeContador()) {
+                    vista.restablecerContador();
+                }else{
+                vista.crearContador();
+                }
+                vista.crearCartas(5, 6);
                 break;
             case "": {
                 if (carta1 == null) {
@@ -43,32 +58,31 @@ public class ControladorMenu implements ActionListener{
                     if (!carta1.isVolteada()) {
                         carta1.voltear();
                         carta1.setVolteada(true);
-                    } 
-                        /*
+                    }
+                    /*
                 Hay que añadir otro if para comprobar si la imagen de la carta 1 volteada coincide con la imagen de la carta 2 volteada y si esto es asi
                 no se hara nada en caso contrario se volveran a poner ambas a false y  a darles la vuelta.
                 Enlace transicion:
                 http://www.jc-mouse.net/java/crea-un-efecto-flip-en-java-swing
                 Enlace imagenes Lazarillo:
                 https://sites.google.com/site/luisrc3blit/imagenes
-                         */
-                    
-                }else {
+                     */
+
+                } else {
                     carta2 = (Carta) ae.getSource();
                     if (!carta2.isVolteada()) {
-                        carta2.voltear();
                         
-                        carta2.setVolteada(true);
                         System.out.println("Hikkajsdkhwkegfgwlvigehocuoyuftikryt9ysrkt9rsjyt9j");
-                        carta2.repaint();
-                    }
-                   try {
-                               Thread.sleep(2000);
-                            } catch (InterruptedException ex) {
-                                Logger.getLogger(ControladorMenu.class.getName()).log(Level.SEVERE, null, ex);
-                      }       
-                    if (carta1.imagenActual().equals(carta2.imagenActual())) {
-                       /* try {
+                        //carta2.repaint();
+                        try {
+                            carta2.voltear();
+                        carta2.setVolteada(true);
+                            Thread.sleep(2000);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(ControladorMenu.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        if (!carta1.imagenActual().equals(carta2.imagenActual())) {
+                            /* try {
                                Thread.sleep(2000);
                             } catch (InterruptedException ex) {
                                 Logger.getLogger(ControladorMenu.class.getName()).log(Level.SEVERE, null, ex);
@@ -77,17 +91,22 @@ public class ControladorMenu implements ActionListener{
                         carta1.setVolteada(false);
                         carta2.voltear();
                         carta2.setVolteada(false);
-                        */
-                        System.out.println("hhhfhj");
+                             */
+                            carta1.voltear();
+                            carta1.setVolteada(false);
+                            carta2.voltear();
+                            carta2.setVolteada(false);
+                            System.out.println("hhhfhj");
+                        }
+                        carta1 = null;
+                        carta2 = null;
                     }
-                    carta1=null;
-                    carta2=null;
-              
-            }
-                break;  
+                }
+
+                break;
             }
             default:
-                System.out.println("No has seleccionado ninguna opción válida");
+                System.out.println("Ha habido un problema");
         }
-    }  
+    }
 }
