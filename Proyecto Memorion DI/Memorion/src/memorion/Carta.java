634d714c0c4e818c7356bc5315e4a571dc758fb1
angelIA98 <45030709+angelIA98@users.noreply.github.com>
2018-11-21 12:53:25 +0100

    Update Carta.java

--- Proyecto Memorion DI/Memorion/src/memorion/Carta.java
+++ Proyecto Memorion DI/Memorion/src/memorion/Carta.java
@@ -5,10 +5,6 @@
  */
 package memorion;
 
-/**
- *
- * @author Sergio
- */
 import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
@@ -17,26 +13,60 @@ import javax.swing.JButton;
 
 /**
  *
- * @author Sergio
+ * @author esmeralda
  */
 public class Carta extends JButton{
     private boolean volteada=false;
     private ImageIcon imagenDorso;
     private ImageIcon imagenFrontal;
     private ControladorMenu controladorMenu;
+    private String [] imagenes={"lazarillo1.jpg","lazarillo2.jpg"};
     private int indice;
     public Carta(ControladorMenu controladorMenu,int indice){
         URL url=getClass().getResource("trasera.png");
         imagenDorso=new ImageIcon(url);
         añadirImagenFrontal();
         establecerIcono();
-        System.out.println(getIcon().toString());
         this.controladorMenu=controladorMenu;
         this.indice=indice;
         addActionListener(controladorMenu);
     }
+
+    public ImageIcon getImagenDorso() {
+        return imagenDorso;
+    }
+
+    public void setImagenDorso(ImageIcon imagenDorso) {
+        this.imagenDorso = imagenDorso;
+    }
+
+    public ImageIcon getImagenFrontal() {
+        return imagenFrontal;
+    }
+
+    public void setImagenFrontal(ImageIcon imagenFrontal) {
+        this.imagenFrontal = imagenFrontal;
+    }
+
+    public ControladorMenu getControladorMenu() {
+        return controladorMenu;
+    }
+
+    public void setControladorMenu(ControladorMenu controladorMenu) {
+        this.controladorMenu = controladorMenu;
+    }
+
+    public String[] getImagenes() {
+        return imagenes;
+    }
+
+    public void setImagenes(String[] imagenes) {
+        this.imagenes = imagenes;
+    }
+    
     public void añadirImagenFrontal(){
-        URL url=getClass().getResource("lazarillo1.jpg");
+        
+        URL url=getClass().getResource(imagenes[(int)(Math.random()*2)]);
         imagenFrontal=new ImageIcon(url);
     } 
     public String imagenActual() {
@@ -73,4 +103,5 @@ public class Carta extends JButton{
     
     
 }
+
 
