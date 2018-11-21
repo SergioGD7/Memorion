f34e1e065c10fb596033890bb82d7bc1a4a2071e
angelIA98 <45030709+angelIA98@users.noreply.github.com>
2018-11-21 12:55:13 +0100

    Update ControladorMenu.java

--- Proyecto Memorion DI/Memorion/src/memorion/ControladorMenu.java
+++ Proyecto Memorion DI/Memorion/src/memorion/ControladorMenu.java
@@ -9,104 +9,114 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.Timer;
 
 /**
  *
  * @author Sergio
  */
-public class ControladorMenu implements ActionListener{
-    
+public class ControladorMenu implements ActionListener {
+
     private Carta carta1 = null;
     private Carta carta2 = null;
     private Vista vista;
-    
-    public ControladorMenu(Vista vista){
-        this.vista=vista;
+    private Timer t;
+
+    public ControladorMenu(Vista vista) {
+        this.vista = vista;
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         System.out.println(ae.getActionCommand());
         switch (ae.getActionCommand()) {
-            case "Fácil":
+            case "Easy":
                 if (vista.existeContador()) {
+                    vista.estadoContador(true);
                     vista.restablecerContador();
-                }else{
-                vista.crearContador();
+                } else {
+                    vista.crearContador();
                 }
                 vista.crearCartas(3, 4);
                 break;
             case "Normal":
                 if (vista.existeContador()) {
+                    vista.estadoContador(true);
                     vista.restablecerContador();
-                }else{
-                vista.crearContador();
+                } else {
+                    vista.crearContador();
                 }
                 vista.crearCartas(4, 5);
                 break;
-            case "Difícil":
+            case "Difficult":
                 if (vista.existeContador()) {
+                    vista.estadoContador(true);
                     vista.restablecerContador();
-                }else{
-                vista.crearContador();
+                } else {
+                    vista.crearContador();
                 }
                 vista.crearCartas(5, 6);
                 break;
             case "": {
-                if (carta1 == null) {
-                    carta1 = (Carta) ae.getSource();
-                    if (!carta1.isVolteada()) {
-                        carta1.voltear();
-                        carta1.setVolteada(true);
-                    }
-                    /*
-                Hay que añadir otro if para comprobar si la imagen de la carta 1 volteada coincide con la imagen de la carta 2 volteada y si esto es asi
-                no se hara nada en caso contrario se volveran a poner ambas a false y  a darles la vuelta.
-                Enlace transicion:
-                http://www.jc-mouse.net/java/crea-un-efecto-flip-en-java-swing
-                Enlace imagenes Lazarillo:
-                https://sites.google.com/site/luisrc3blit/imagenes
-                     */
-
-                } else {
-                    carta2 = (Carta) ae.getSource();
-                    if (!carta2.isVolteada()) {
-                        
-                        System.out.println("Hikkajsdkhwkegfgwlvigehocuoyuftikryt9ysrkt9rsjyt9j");
-                        //carta2.repaint();
-                        try {
-                            carta2.voltear();
-                        carta2.setVolteada(true);
-                            Thread.sleep(2000);
-                        } catch (InterruptedException ex) {
-                            Logger.getLogger(ControladorMenu.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        if (!carta1.imagenActual().equals(carta2.imagenActual())) {
-                            /* try {
-                               Thread.sleep(2000);
-                            } catch (InterruptedException ex) {
-                                Logger.getLogger(ControladorMenu.class.getName()).log(Level.SEVERE, null, ex);
-                            
-                        carta1.voltear();
-                        carta1.setVolteada(false);
-                        carta2.voltear();
-                        carta2.setVolteada(false);
-                             */
-                            carta1.voltear();
-                            carta1.setVolteada(false);
-                            carta2.voltear();
-                            carta2.setVolteada(false);
-                            System.out.println("hhhfhj");
-                        }
-                        carta1 = null;
-                        carta2 = null;
-                    }
-                }
-
+                voltearCartas(ae);
                 break;
             }
             default:
                 System.out.println("Ha habido un problema");
         }
+    }
+
+    public void voltearCartas(ActionEvent ae) {
+        if (carta1 == null) {
+            carta1 = (Carta) ae.getSource();
+            if (!carta1.isVolteada()) {
+                carta1.voltear();
+                carta1.setVolteada(true);
+            }
+            /*
+                
+                Enlace transicion:
+                http://www.jc-mouse.net/java/crea-un-efecto-flip-en-java-swing
+                Enlace imagenes Lazarillo:
+                https://sites.google.com/site/luisrc3blit/imagenes
+             */
+
+        } else {
+            carta2 = (Carta) ae.getSource();
+            if (!carta2.isVolteada()) {
+
+                //carta2.repaint();
+                carta2.voltear();
+                carta2.setVolteada(true);
+                comprobarCartasIguales();
+
+            }
+        }
+    }
+
+    public void comprobarCartasIguales() {
+        Timer t = new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (!carta1.imagenActual().equals(carta2.imagenActual())) {
+                    carta1.voltear();
+                    carta1.setVolteada(false);
+                    carta2.voltear();
+                    carta2.setVolteada(false);
+                } else {
+                    carta1 = null;
+                    carta2 = null;
+                    if (vista.partidaAcabada()) {
+                        System.out.println("Partida Acabada");
+                        vista.estadoContador(false);
+                    }
+                }
+                carta1 = null;
+                carta2 = null;
+            }
+        });
+        t.start();
+        t.setRepeats(false);
+       
     }
 }
