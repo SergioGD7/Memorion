7b5d72ff25401a627d7b4a5ab1df9bc4b6738a85
angelIA98 <45030709+angelIA98@users.noreply.github.com>
2018-11-21 12:39:02 +0100

    Update Vista.java

--- Proyecto Memorion DI/Memorion/src/memorion/Vista.java
+++ Proyecto Memorion DI/Memorion/src/memorion/Vista.java
@@ -9,7 +9,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.Label;
 import java.awt.Menu;
 import java.awt.MenuBar;
@@ -18,163 +17,177 @@ import java.awt.MenuShortcut;
 import java.awt.Panel;
 import java.awt.event.KeyEvent;
 import java.net.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 /**
  *
  * @author Sergio
  */
-public class Vista extends JFrame{
-    
-    private ControladorMenu controladorMenu;
-    
-    private MenuBar menuJuego;
-    private Menu partidaNueva;
-    private MenuItem facil,normal,dificil;
-    private MenuShortcut atajoFacil,atajoNormal,atajoDificil;
-    private JButton[][] cartas;
-    private ImageIcon imagentrasera;
-    private Panel panelCartas;
-    private Label nombreContador,contador,labelInicio;
-    private int minutos=0,segundos=0,contadorCartas=0;
-    private String min,seg;
+public class Vista extends JFrame {
 
-    
-    public Vista(){
+    private MenuBar menuJuego;
+    private Menu partidaNueva, cargar, guardar;
+    private MenuItem facil, normal, dificil,cargarPartida, guardarPartida;
+    private MenuShortcut atajoFacil, atajoNormal, atajoDificil;
+    private Carta[][] cartas;
+    private ControladorMenu controladorMenu;
+    private Panel panelCartas;
+    private Label l1;
+    private Contador c;
+
+    public Vista() {
+        setTitle("MEMORION");
+        this.controladorMenu = new ControladorMenu(this);
         crearMenu();
-        crearInterfaz();
-        crearPanel();
+
+        //crearInterfaz();
+        crearPanelInicial();
+
+        setSize(1366, 768);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        System.out.println("jjj");
     }
-    
-    public void crearMenu(){
-         //Creamos barra de Menu
+
+    /**
+     * En este método creamos un panel inicial que posteriormente sera
+     * sustituido por el juego.Aquí se le explica al usuario que tiene que
+     * elegir entre iniciar una partida o cargar una ya existente.
+     */
+    public void crearPanelInicial() {
+        panelCartas = new Panel();
+        JTextArea textArea = new JTextArea("Elija en el menu superior la opción que desea ya sea iniciar una partida o cargar una ya existente.");
+        textArea.setBackground(new Color(29, 187, 240));
+        textArea.setFont(new Font("Courier", Font.ITALIC, 25));
+        textArea.setForeground(Color.GRAY);
+        panelCartas.setBackground(new Color(29, 187, 240));
+        panelCartas.add(textArea);
+        add(BorderLayout.CENTER, panelCartas);
+    }
+
+    public void crearMenu() {
+        //Creamos barra de Menu
         menuJuego = new MenuBar();
-        
         //Creamos los 2 elementos menu
-        partidaNueva= new Menu("New Game");
-        
-        atajoFacil=new MenuShortcut(KeyEvent.VK_E,true);
-        atajoNormal=new MenuShortcut(KeyEvent.VK_N,true);
-        atajoDificil=new MenuShortcut(KeyEvent.VK_D,true);
-        
+        partidaNueva = new Menu("Nueva partida");
+        atajoFacil = new MenuShortcut(KeyEvent.VK_E, true);
+        atajoNormal = new MenuShortcut(KeyEvent.VK_N, true);
+        atajoDificil = new MenuShortcut(KeyEvent.VK_D, true);
+
         //Creamos el elemento menuItem y lo configuramos
-        facil = new MenuItem("Easy",atajoFacil);
-        normal = new MenuItem("Normal",atajoNormal);
-        dificil = new MenuItem("Dificult",atajoDificil);
-        
+        facil = new MenuItem("Easy", atajoFacil);
+        normal = new MenuItem("Normal", atajoNormal);
+        dificil = new MenuItem("Difficult", atajoDificil);
+
         partidaNueva.add(facil);
         partidaNueva.add(normal);
         partidaNueva.add(dificil);
-        
-        facil.addActionListener(new ControladorMenu(this));
-        normal.addActionListener(new ControladorMenu(this));
-        dificil.addActionListener(new ControladorMenu(this));
-        
+
+        facil.addActionListener(controladorMenu);
+        normal.addActionListener(controladorMenu);
+        dificil.addActionListener(controladorMenu);
+
         //Añadimos los elementos Menu a la barra de menu
         menuJuego.add(partidaNueva);
+        cargar = new Menu("Load");
+        cargarPartida=new MenuItem("Load Game");
+        cargar.addActionListener(controladorMenu);
+        cargar.add(cargarPartida);
         
+        guardar = new Menu("Save");
+        guardarPartida=new MenuItem("Save Game");
+        guardar.addActionListener(controladorMenu);
+        guardar.add(guardarPartida);
+        
+        menuJuego.add(cargar);
+        menuJuego.add(guardar);
         //Establecemos la barra de menu al Frame
         this.setMenuBar(menuJuego);
-        
+
     }
-    
-    public void crearInterfaz(){
-        setTitle("MEMORION");
-        
-        controladorMenu=new ControladorMenu(this);
-        //Cambiamos el icono de la aplicación
-        Image icon = new ImageIcon(getClass().getResource("trasera.png")).getImage();
-        setIconImage(icon);
-        
-        setLayout(null);
-        
-        setResizable(false);
-        setSize(1366,768);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    public void voltear() {
+        /*prponer idea de cambiar el array bidimensional a uno unidimensional lo que facilitaria este metodo ya que pasando por parametro
+        el indice de la carta se elige la carta que voltear facilmente
+        cartas[indice].voltear();
+         */
     }
+
     
-    public void crearPanel(){
-        panelCartas=new Panel();
-        labelInicio=new Label("Elija en el menu superior la opción que desea ya sea iniciar una partida o cargar una ya existente.");
-        //labelInicio.setBackground(Color.BLUE);
-        labelInicio.setFont(new Font("Courier", Font.ITALIC, 25));
-        labelInicio.setForeground(Color.GRAY);
-        panelCartas.setBackground(Color.BLUE);
-        panelCartas.setBounds(10,10,1000,700);
-        panelCartas.add(labelInicio);
-        add(BorderLayout.CENTER,panelCartas);
-        /*panelCartas.setBounds(10,10,1000,700);
-        add(BorderLayout.CENTER,panelCartas);*/
-    }
-    
-    public void crearCartas(int filas,int columnas){
+    public void crearCartas(int filas, int columnas) {
+        int contador = 0;
         remove(panelCartas);
-        panelCartas=new Panel();
-        panelCartas.setBounds(10,10,1000,700);
-        cartas=new Carta[filas][columnas];
-        URL url=getClass().getResource("trasera.png");
-        imagentrasera=new ImageIcon(url);
-        panelCartas.setLayout(new GridLayout(filas,columnas));
-        
-        for(int i=0;i<filas;i++){
-            for(int j=0;j<columnas;j++){
-                System.out.println("carta "+i+"|"+j);
-                cartas[i][j]=new Carta(controladorMenu,contadorCartas);
+        panelCartas = new Panel();
+        panelCartas.setSize(800, 600);
+        cartas = new Carta[filas][columnas];
+
+        panelCartas.setLayout(new GridLayout(filas, columnas));
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.println("carta " + i + "|" + j);
+                cartas[i][j] = new Carta(controladorMenu, contador);
+
+                //cartas[i][j].setIcon(imagentrasera);
                 panelCartas.add(cartas[i][j]);
-                contadorCartas++;
+                contador++;
             }
-            
+
         }
-       add(BorderLayout.CENTER,panelCartas);
-       //contador=new Contador();
-       //crearContador();
-       //actualizarContador();
-       setVisible(true);
-       contadorCartas=0;
-    }
-    
-    public void crearContador(){
-        nombreContador=new Label("Time");
-        nombreContador.setBounds(1150,2,150,50);
-        nombreContador.setFont(new Font("Serif", Font.BOLD, 48));
-        contador=new Label();
-        contador.setBounds(1150,80,150,50);
-        contador.setFont(new Font("Serif", Font.BOLD, 48));
-        
-        add(nombreContador);
-        add(contador);
-    }
-    
-   /* public void actualizarContador(){
-        for(segundos=0;segundos>=0;segundos++){
-            try {
-                    System.out.println(minutos+":"+segundos);
-                    min=Integer.toString(minutos);
-                    seg=Integer.toString(segundos);
-                    //contador.setText(min+":"+seg);
-                    //Thread.sleep(1000);
-                    if(segundos==59){
-                        minutos=minutos+1;
-                        segundos=-1;
-                    }
-                    if(minutos<10 && segundos>=0 && segundos<10){
-                        contador.setText("0"+min+":0"+seg);
-                    }else if(minutos<10){
-                        contador.setText("0"+min+":"+seg);
-                    }else{
-                        contador.setText(min+":"+seg);
-                    }
-                    Thread c=new Thread();
-                    c.sleep(1000);
-                } catch (InterruptedException ex) {
-                    System.out.println("Error");
-                }
-            
-        }
+        add(BorderLayout.CENTER, panelCartas);
+        // Thread t=new Thread(new Contador());
+        // t.start();
+
         setVisible(true);
+        contador = 0;
+    }
+
+    public synchronized void crearContador() {
+        if (!existeContador()) {
+            c = new Contador(this);
+        }
+//c.start();
+
+        Panel p2 = new Panel();
+        l1 = new Label("Time: 00:00");
+        p2.add(l1);
+        add(BorderLayout.NORTH, p2);
+
+    }
+
+    public void actualizarMarcador(String tiempo) {
+        l1.setText("Time: " + tiempo);
+        //setVisible(true);
+    }
+
+    public boolean existeContador() {
+        if (c == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void restablecerContador() {
+        c.restablecerContador();
         
-    }*/
+    }
+    public boolean partidaAcabada(){
+        boolean acabada=true;
+        for (int i = 0; i < cartas.length; i++) {
+            for (int j = 0; j < cartas[i].length; j++) {
+                if(!cartas[i][j].isVolteada()){
+                    acabada=false;
+                }
+            }            
+        }
+        return acabada;
+    }
+    public void estadoContador(boolean estado){
+        c.setContar(estado);
+       
+    }
+    
 }
+
