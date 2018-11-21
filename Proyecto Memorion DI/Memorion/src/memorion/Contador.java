46e62bae5f850a6dbb5cf59cb29c7616d4d7571c
angelIA98 <45030709+angelIA98@users.noreply.github.com>
2018-11-21 12:50:15 +0100

    Update Contador.java

--- Proyecto Memorion DI/Memorion/src/memorion/Contador.java
+++ Proyecto Memorion DI/Memorion/src/memorion/Contador.java
@@ -12,43 +12,56 @@ import java.awt.Panel;
  *
  * @author Sergio
  */
-public class Contador extends Panel implements Runnable{
-    
-    private int minutos=0,segundos=0,contadorCartas=0;
-    private String min,seg;
-    private Label contador;
+public class Contador extends Thread {
 
-    Contador() {
-        
+    private Vista v;
+    private int minutos = 0, segundos = 0, contadorCartas = 0;
+    private String min, seg;
+    private String contador;
+    private boolean contar=true;
+
+    Contador(Vista v) {
+        this.v = v;
+        this.start();
     }
-    
+
     @Override
     public void run() {
-        for(segundos=0;segundos>=0;segundos++){
+        while (true) {
+
+            if(contar){
+            System.out.println(minutos + ":" + segundos);
+            min = Integer.toString(minutos);
+            seg = Integer.toString(segundos);
+            if (segundos == 59) {
+                minutos = minutos + 1;
+                segundos = -1;
+            }
+            if (minutos < 10 && (segundos == 0 || segundos < 10)) {
+                contador = "0" + min + ":0" + seg;
+            } else if (minutos < 10) {
+                contador = "0" + min + ":" + seg;
+            } else {
+                contador = min + ":" + seg;
+            }
+            v.actualizarMarcador(contador);          
+            segundos++;
             try {
-                    System.out.println(minutos+":"+segundos);
-                    min=Integer.toString(minutos);
-                    seg=Integer.toString(segundos);
-                    //contador.setText(min+":"+seg);
-                    //Thread.sleep(1000);
-                    if(segundos==59){
-                        minutos=minutos+1;
-                        segundos=-1;
-                    }
-                    if(minutos<10 && segundos>=0 && segundos<10){
-                        contador.setText("0"+min+":0"+seg);
-                    }else if(minutos<10){
-                        contador.setText("0"+min+":"+seg);
-                    }else{
-                        contador.setText(min+":"+seg);
-                    }
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    System.out.println("Error");
-                }
-            
-        }
-        setVisible(true);
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                System.out.println("Error");
+            }
+            }
+        }       
     }
-    
+
+    public void restablecerContador() {
+        minutos = 0;
+        segundos = 0;
+    }
+
+    public void setContar(boolean contar) {
+        this.contar = contar;
+    }
+
 }
